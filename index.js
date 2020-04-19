35ae492c1ad1709362ddc32a0e27b99eced98c44
udaramadhushan <udaram188@gmail.com>
2020-04-20 00:09:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-$("#search-bar").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-  
+$(document).ready(function(){
 
- 
-   $filteredCards = $('.card').filter(function() {
+  $searchbar = $("#search-bar");
+
+  $searchbar.on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $filteredCards = $('.card').filter(function() {
       $(this).toggle($(this).find('.card-title').text().toLowerCase().indexOf(value) > -1);
     });
 
-    $filteredTitles = $filteredCards.parents().children().find('.title').hide();
     
-  });
+   
+    
+  }); 
+
+  
+})
