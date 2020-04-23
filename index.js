657c09048ef2efd24645eea82e0b80dfcf27958a
udaramadhushan <udaram188@gmail.com>
2020-04-23 19:14:54 +0530

    modifications done

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 
-  $('#myTab a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  })
+  $searchbar = $("#search-bar");
 
+  $searchbar.on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $filteredCards = $('.card').filter(function() {
+      $(this).toggle($(this).find('.card-title').text().toLowerCase().indexOf(value) > -1);
+      $(this).toggle($(this).find('.card-text').text().toLowerCase().indexOf(value) > -1);
 
+    });
+
+    
+   
+    
+  }); 
+
+  
 })
