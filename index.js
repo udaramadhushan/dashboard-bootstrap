819138d5fb44639949a7adfde3f46612ef42ad09
udaramadhushan <udaram188@gmail.com>
2020-04-23 08:34:42 +0530

    footer changed , search queries work for both titles and description

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
     var value = $(this).val().toLowerCase();
     $filteredCards = $('.card').filter(function() {
       $(this).toggle($(this).find('.card-title').text().toLowerCase().indexOf(value) > -1);
+      $(this).toggle($(this).find('.card-text').text().toLowerCase().indexOf(value) > -1);
+
     });
 
     
