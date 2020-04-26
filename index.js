f830366856aa01eeb353a930eb2f51b6c5a0fdfb
udaramadhushan <udaram188@gmail.com>
2020-04-26 14:44:17 +0530

    icons are loaded from json payload backend file icon_data.json

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
+
 $(document).ready(function(){
 
   $searchbar = $("#search-bar");
   $titles = $('.title');
-  $cards = $('.card');
+ 
 
   $('#no-result').hide();
 
   $.getJSON("icon_data.json",function( json ) {
-    $titles.each(function(index,element){
-
-      $(element).text(Object.keys(json)[index]);
-    })
-   
+  
+      fillIcons('.student-services',json.studentServices) 
+      fillIcons('.staff-services',json.staffServices) 
+      fillIcons('.account-management',json.accountManagement) 
+      fillIcons('.public-services',json.publicServices) 
    });
 
+   const fillIcons = (title, objectArray) =>{
+
+    objectArray.forEach( function(object,index){
+    newCard = $('#card-templete').clone();
+
+    $(newCard).removeAttr('id');
+    $(newCard).children('.card-body').find('.card-title').html(object.title);
+    $(newCard).children('.card-body').find('.card-text').html(object.description);
+    $(newCard).children('.card-icon').addClass(object.icon);
+    $(newCard).children('.card-footer').find('.card-url').attr("href", object.URL);
+    $(title).closest('div').find('.card-panel').append(newCard);
+    })
+  }
+ 
+ 
 
 
   $searchbar.on("keyup", function() {
+
+   
+
     var value = $(this).val().toLowerCase();
-    $filteredCards = $cards.filter(function() {
+    $filteredCards = $('.card-panel').find('.card').filter(function() {
       $(this).toggle($(this).find('.card-title').text().toLowerCase().indexOf(value) > -1);
       $(this).toggle($(this).find('.card-text').text().toLowerCase().indexOf(value) > -1);
 
     });
-
-  
-  
 
    $titles.each(function(index,element) {
     number = $(element).closest('div').find('div.card:visible').length;
@@ -33,15 +49,16 @@ $(document).ready(function(){
    });
 
    ($("div.card:visible").length === 0) ?   $("#no-result").show() :  $("#no-result").hide();
-  
-     
-    
 
   
   }); 
 
-  
+
+
+
+ 
  
 
  
 })
+
